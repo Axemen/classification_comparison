8b741f6af8b2b6fe236f3d3e15db25797387a7ca
Jonathan Randolph <Jrandolph011@gmail.com>
2020-08-29 21:13:26 -0500

    added documentation for new approach"

--- compare_models.py
+++ compare_models.py
@@ -1,50 +1,72 @@
 import webbrowser
 from typing import List
 
-import sklearn
+import sklearn.metrics as skmetrics
+
 
 """ 
 Expected code to run 
 
 models = [
-    LinearRegression(),
-    RandomForest(),
+    ("Linear Regression", LinearRegression()),
+    ("Random Forest", RandomForest()),
     ...
 ]
 
-compare_models(models)
+create_report(models)
 
 This will spin up the local server and open the web browser. 
 """
+def create_report(models, X, y, random_state=42):
+    # TODO
+    pass
 
-def compare_models(models, X, y, 
-                    random_state=42, 
-                    metrics=['recall_score', 'precision_score']):
-    # TODO 
+def compare_models(models, X, y,
+                   random_state=42,
+                   custom_metrics=None):
+    """ 
+    Takes in a list of models and calculates the metrics for said models
+
+    models = [
+        ("Logistic Regression", LogisticRegression()),
+        (<name>, <model>)
+    ]
+
+    X = Data 
+    y = Target
+
+    custom_metrics = [
+        ("recall", sklearn.metrics.recall),
+        (<name>, metric)
+    ]
+    """
+    # TODO
     results = []
-    (X_train, X_test, 
+    (X_train, X_test,
         y_train, y_test) = train_test_split(X, y, random_state=random_state)
 
-    for model in models:
+    for name, model in models:
         model.fit(X_train, y_train)
         preds = model.predict(X_test)
-        results.append(calc_metrics(y_test, preds, metrics))
+        results.append({
+            "name": name,
+            "metrics": calc_metrics(y_test, preds, metrics)
+        })
     return results
 
-def calc_metrics(y_true, y_pred, metrics):
+
+def calc_custom_metrics(y_true, y_pred, metrics):
     """
     Takes in the true and predicted value and returns the 
     metrics passed inside of the metrics parameter.
 
-    Metrics must be in the form of the SK-Learn function name. 
+    Metrics can be passed through in the form of 
+    (<name>, func_ref)
+
+    The function reference given must accept the arguments y_true, and y_pred
     """
-    results = {}
+    return {name: metric(y_true, y_pred) for name, metric in metrics}
 
-    for metric, kw in metrics:
-        func = getattr(sklearn.metrics, metric)
-        results[metric] = func(y_true, y_pred, **kw)
-
-    return results
 
 if __name__ == "__main__":
     # ! This is nothing but a test case
@@ -56,11 +78,11 @@ if __name__ == "__main__":
     import numpy as np
 
     models = [
-        LogisticRegression(),
-        RandomForestClassifier(),
-        DummyClassifier()
+        ("LR", LogisticRegression()),
+        ("RF", RandomForestClassifier()),
+        ("DC", DummyClassifier())
     ]
-    
+
     X = np.random.random_sample((100, 5))
     y = np.random.randint(5, size=(100,))
 
@@ -70,11 +92,6 @@ if __name__ == "__main__":
     m.fit(X_train, y_train)
 
     preds = m.predict(X_test)
-
-    metrics = [
-        ('recall_score', {"average":None}),
-        ('precision_score', {"average":None})
-    ]
 
     results = calc_metrics(y_test, preds, metrics)
     print(results)
